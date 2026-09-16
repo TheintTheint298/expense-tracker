87c960cbcb607a189dd81ffa095db1fee357b865
TheintTheint298 <theinttheintsoehtet@gmail.com>
2026-09-16 19:41:09 +0630

    feat: composition (children props)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.items.map((expense) => (
         <ExpenseItem
           id={expense.id}
@@ -12,7 +13,7 @@ function Expenses(props) {
           date={expense.date}
         />
       ))}
-    </div>
+    </Card>
   );
 }
 
